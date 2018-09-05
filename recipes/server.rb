7fd884faaaab3b6d71795ef80e4d0efcbff4b570
root <root@ip-172-31-23-82.ap-south-1.compute.internal>
2018-09-05 11:29:28 +0000

    Replaced Template with Cookbook_file

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,8 +2,12 @@ package 'httpd' do
 	action :install
 end
 
-template '/var/www/html/index.html' do
-	source 'index.html.erb'
+cookbook_file '/var/www/html/index.html' do
+ source 'index.html'
+ action :create
+
+#template '/var/www/html/index.html' do
+#	source 'index.html.erb'
 end
 
 service 'httpd' do
