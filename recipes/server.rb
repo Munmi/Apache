b4b0f60264e93b59a2595c6534a3240e0ff4ae81
root <root@ip-172-31-23-82.ap-south-1.compute.internal>
2018-09-05 12:16:27 +0000

    Adding remote file

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,12 +2,16 @@ package 'httpd' do
 	action :install
 end
 
-cookbook_file '/var/www/html/index.html' do
- source 'index.html'
- action :create
+#cookbook_file '/var/www/html/index.html' do
+# source 'index.html'
+ #action :create
 
-#template '/var/www/html/index.html' do
-#	source 'index.html.erb'
+remote_file '/var/www/html/Munmi.png' do
+	source "https://s3.ap-south-1.amazonaws.com/munmitest/DSC_0008.JPG"
+	action :create
+end
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
 end
 
 service 'httpd' do
